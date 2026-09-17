efddf10e5fd155b807f0372a1be7ca9d86dde5e0
harishk2025aids-lgtm <harish.k2025aids@sece.ac.in>
2026-09-17 06:41:17 +0000

    commit

---
+++ project176/src/main/java/com/harish/project176/controller/usercontroller.java
@@ -0,0 +1,27 @@
+package com.harish.project176.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+@RestController 
+@RequestMapping("/api/user")
+public class usercontroller {
+    private record user(String id, String firstName, String lastName, String email) {
+    }
+
+    @GetMapping("")
+   public List<user> getAll()
+   {
+    List<user> users = List.of(
+        new user("123", "Krish", "Karthik", "krish@gmail.com"),
+        new user("123", "Krish", "Karthik", "krish@gmail.com")
+    );
+
+   return users;
+   }
+
+
+}
+      
