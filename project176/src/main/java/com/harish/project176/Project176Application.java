8cfb7833a99b23a1682ff55bf1a85c5058e53ddf
harishk2025aids-lgtm <harish.k2025aids@sece.ac.in>
2026-09-17 23:08:02 +0530

    commit

---
+++ project176/src/main/java/com/harish/project176/Project176Application.java
@@ -0,0 +1,13 @@
+package com.harish.project176;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project176Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project176Application.class, args);
+	}
+
+}
---
+++ project176/src/test/java/com/harish/project176/Project176ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harish.project176;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project176ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
