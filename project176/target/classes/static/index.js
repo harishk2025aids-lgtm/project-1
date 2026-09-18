80389c78415771f9443348be71c95ade4ebe642a
harishk2025aids-lgtm <harish.k2025aids@sece.ac.in>
2026-09-18 09:52:32 +0000

    c

--- project176/target/classes/static/index.js
+++ project176/target/classes/static/index.js
@@ -1,13 +1,43 @@
-const dataFromServer="Hello world";
-const primaryHeaderElement=document.getElementById("primary-header");
+const state = {
+    users: [],
+};
 
-primaryHeaderElement.textContent=dataFromServer;
+function renderUsers(){
+    const usersTableElement = document.getElementById("users-list");
+    const usersTableBodyElement = document.getElementById("users-list-body");
+
+    for(const user of state.users){
+        const trElement = document.createElement("tr");
+
+        const idElement = document.createElement("td");
+        idElement.textContent = user.id;
+        trElement.appendChild(idElement);
+
+        const firstNameElement = document.createElement("td");
+        firstNameElement.textContent = user.firstName;
+        trElement.appendChild(firstNameElement);
+
+        const lastNameElement = document.createElement("td");
+        lastNameElement.textContent = user.lastName;
+        trElement.appendChild(lastNameElement);
+
+        const emailElement = document.createElement("td");
+        emailElement.textContent = user.email;
+        trElement.appendChild(emailElement);
+
+        usersTableBodyElement.appendChild(trElement);
+    }
+    
+}
+async function fetchUsers(){
+    const userResponse = await fetch("/api/user");
+    const usersList = await userResponse.json();
+    state.users = usersList;
+    
+    renderUsers();
+}
 
 function main(){
-    console.log("Main function executed");
+    fetchUsers();
 }
-main();
-
-const arrowFn = () =>  "hello";
-
-console.log(arrowFn());
+document.addEventListener("DOMContentLoaded", main);
