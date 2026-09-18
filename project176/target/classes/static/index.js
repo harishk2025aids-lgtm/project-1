f1057c3e46284ea54f033ae89a0135b1e4f1d08c
harishk2025aids-lgtm <harish.k2025aids@sece.ac.in>
2026-09-18 07:03:47 +0000

    c

--- project176/target/classes/static/index.js
+++ project176/target/classes/static/index.js
@@ -1,1 +1,13 @@
-console.log("helloworld");
+const dataFromServer="Hello world";
+const primaryHeaderElement=document.getElementById("primary-header");
+
+primaryHeaderElement.textContent=dataFromServer;
+
+function main(){
+    console.log("Main function executed");
+}
+main();
+
+const arrowFn = () =>  "hello";
+
+console.log(arrowFn());
